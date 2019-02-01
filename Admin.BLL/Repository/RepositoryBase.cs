e313b4fe5221358ee3c4afb08837d56c9b088d6d
Alper Dağlı <alper379@gmail.com>
2019-02-01 11:26:06 +0300

    Ürün ekleneme tamamlandı.

--- Admin.BLL/Repository/RepositoryBase.cs
+++ Admin.BLL/Repository/RepositoryBase.cs
@@ -18,8 +18,8 @@ namespace Admin.BLL.Repository
         {
             DbContext = DbContext ?? new MyContext();
             TimeSpan dd = DateTime.Now - DbContext.InstanceDate;
-            if(IsDisposed) DbContext = new MyContext();
-            if(dd.TotalMinutes > 30) DbContext = new MyContext();
+            if (IsDisposed) DbContext = new MyContext();
+            if (dd.TotalMinutes > 30) DbContext = new MyContext();
             DbObject = DbContext.Set<T>();
         }
 
@@ -27,86 +27,75 @@ namespace Admin.BLL.Repository
         {
             return DbObject.ToList();
         }
-
         public List<T> GetAll(Func<T, bool> predicate)
         {
             return DbObject.Where(predicate).ToList();
         }
-
         public async Task<List<T>> GetAllAsync()
         {
             return await DbObject.ToListAsync();
         }
-
         public async Task<List<T>> GetAllAsync(Func<T, bool> predicate)
         {
             return await DbObject.Where(predicate).AsQueryable().ToListAsync();
         }
-
         public T GetById(params object[] keys)
         {
             return DbObject.Find(keys);
         }
-
         public async Task<T> GetByIdAsync(params object[] keys)
         {
             return await DbObject.FindAsync(keys);
         }
-
         public int Insert(T entity)
         {
             DbObject.Add(entity);
             return DbContext.SaveChanges();
         }
-
         public void InsertForMark(T entity)
         {
             DbObject.Add(entity);
         }
-
         public async Task<int> InsertAsync(T entity)
         {
             DbObject.Add(entity);
             return await DbContext.SaveChangesAsync();
         }
-
         public int Delete(T entity)
         {
             DbObject.Remove(entity);
             return DbContext.SaveChanges();
         }
-
         public void DeleteForMark(T entity)
         {
             DbObject.Remove(entity);
         }
-
         public async Task<int> DeleteAsync(T entity)
         {
             DbObject.Remove(entity);
             return await DbContext.SaveChangesAsync();
         }
-
-        public int Update()
+        public int Save()
         {
             return DbContext.SaveChanges();
         }
-
-        public async Task<int> UpdateAsync()
+        public async Task<int> SaveAsync()
         {
             return await DbContext.SaveChangesAsync();
         }
-
-        public void Update(T entity)
+        public int Update(T entity)
         {
             DbObject.Attach(entity);
             DbContext.Entry(entity).State = EntityState.Modified;
             entity.UpdatedDate = DateTime.Now;
-            this.Update();
+            return this.Save();
         }
 
+        public IQueryable<T> Queryable()
+        {
+            return DbObject;
+        }
         public bool IsDisposed { get; set; }
-
         public void Dispose()
         {
             GC.SuppressFinalize(this);
---
+++ Admin.Models/Models/BarcodeResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Models.Models
+{
+    public class BarcodeResult
+    {
+        public string Barcode { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
