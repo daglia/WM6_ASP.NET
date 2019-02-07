f60c59451c253207ff7fe0f5d84cd9f228f6644a
Alper Dağlı <alper379@gmail.com>
2019-02-07 13:19:14 +0300

    Şifre kurtarma özelliği eklendi.

--- Admin.Web.UI/Controllers/AccountController.cs
+++ Admin.Web.UI/Controllers/AccountController.cs
@@ -29,6 +29,7 @@ namespace Admin.Web.UI.Controllers
         // GET: Account
         public ActionResult Index()
         {
+            //HttpContext.User.Identity.GetUserId();
             if (HttpContext.GetOwinContext().Authentication.User.Identity.IsAuthenticated)
                 return RedirectToAction("Index", "Home");
             return View();
@@ -327,6 +328,63 @@ namespace Admin.Web.UI.Controllers
             catch (Exception ex)
             {
                 ViewBag.Message = "<span class='alert alert-danger'>Aktivasyon işleminde bir hata oluştu</span>";
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult RecoverPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public async Task<ActionResult> RecoverPassword(RecoverPasswordViewModel model)
+        {
+            try
+            {
+                var userStore = NewUserStore();
+                var userManager = NewUserManager();
+                var user = await userStore.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, $"{model.Email} mail adresine kayıtlı bir üyeliğe erişilemedi");
+                    return View(model);
+                }
+
+                var newPassword = StringHelpers.GetCode().Substring(0, 6);
+                await userStore.SetPasswordHashAsync(user, userManager.PasswordHasher.HashPassword(newPassword));
+                var result = userStore.Context.SaveChanges();
+                if (result == 0)
+                {
+                    TempData["Model"] = new ErrorViewModel()
+                    {
+                        Text = $"Bir hata oluştu",
+                        ActionName = "RecoverPassword",
+                        ControllerName = "Account",
+                        ErrorCode = 500
+                    };
+                    return RedirectToAction("Error", "Home");
+                }
+
+                var emailService = new EmailService();
+                var body = $"Merhaba <b>{user.Name} {user.Surname}</b><br>Hesabınızın parolası sıfırlanmıştır<br> Yeni parolanız: <b>{newPassword}</b> <p>Yukarıdaki parolayı kullanarak sistemize giriş yapabilirsiniz.</p>";
+                emailService.Send(new IdentityMessage() { Body = body, Subject = $"{user.UserName} Şifre Kurtarma" }, user.Email);
+            }
+            catch (Exception ex)
+            {
+                TempData["Model"] = new ErrorViewModel()
+                {
+                    Text = $"Bir hata oluştu {ex.Message}",
+                    ActionName = "RecoverPassword",
+                    ControllerName = "Account",
+                    ErrorCode = 500
+                };
+                return RedirectToAction("Error", "Home");
             }
 
             return View();
--- Admin.Web.UI/Controllers/HomeController.cs
+++ Admin.Web.UI/Controllers/HomeController.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Admin.Web.UI.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-
-        [HttpPost]
+        [AllowAnonymous]
         public ActionResult Error()
         {
 
