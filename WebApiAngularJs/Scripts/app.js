565575a1939382aa985148e5fc340e8f1d4b01e8
Alper Dağlı <alper379@gmail.com>
2019-02-27 14:54:18 +0300

    AngularJs'e giriş.

---
+++ WebApiAngularJs/Scripts/app.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="angular.js" />
+
+var app = angular.module("myApp", []);
+
+app.controller("ProductCtrl", function($scope) {
+    $scope.urunler = [];
+    function init() {
+        var data = JSON.parse(localStorage.getItem("urunler"));
+        $scope.urunler = data === null ? [] : data;
+    }
+
+    $scope.ekle = function() {
+        $scope.urunler.push({
+            urunAdi: $scope.yeni.urunAdi,
+            fiyat: $scope.yeni.fiyat,
+            eklenmeZamani: new Date()
+        });
+        $scope.yeni.urunAdi = "";
+        $scopeyeni.yeni.fiyat = "";
+        localStorage.setItem("urunler",JSON.stringify($scope.urunler));
+    };
+
+    init();
+});
