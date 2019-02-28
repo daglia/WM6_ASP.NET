873ad2dc867c630cb90aa43bbbeaa0b5020cc684
Alper Dağlı <alper379@gmail.com>
2019-02-28 09:51:00 +0300

    Nesne ekleme, silme ve doğrulama işlemleri.

--- WebApiAngularJs/Scripts/app.js
+++ WebApiAngularJs/Scripts/app.js
@@ -2,23 +2,44 @@
 
 var app = angular.module("myApp", []);
 
-app.controller("ProductCtrl", function($scope) {
+app.controller("ProductCtrl", function ($scope) {
     $scope.urunler = [];
+
     function init() {
         var data = JSON.parse(localStorage.getItem("urunler"));
         $scope.urunler = data === null ? [] : data;
     }
-
-    $scope.ekle = function() {
+    $scope.ekle = function () {
         $scope.urunler.push({
+            id: guid(),
             urunAdi: $scope.yeni.urunAdi,
             fiyat: $scope.yeni.fiyat,
             eklenmeZamani: new Date()
         });
         $scope.yeni.urunAdi = "";
-        $scopeyeni.yeni.fiyat = "";
-        localStorage.setItem("urunler",JSON.stringify($scope.urunler));
+        $scope.yeni.fiyat = "";
+        localStorage.setItem("urunler", JSON.stringify($scope.urunler));
     };
+
+    $scope.sil = function(id) {
+        for (var i = 0; i < $scope.urunler.length; i++) {
+            var data = $scope.urunler[i];
+            if (id === data.id) {
+                $scope.urunler.splice(i, 1);
+                break;
+            }
+        }
+        localStorage.setItem("urunler", JSON.stringify($scope.urunler));
+    }
+
+    function guid() {
+        function S4() {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        }
+
+        // then to call it, plus stitch in '4' in the third group
+        return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+    }
 
     init();
 });
