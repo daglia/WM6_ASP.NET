89f16b433de24a0da133d842e0d5abfb404e6163
Alper Dağlı <alper379@gmail.com>
2019-03-04 11:36:03 +0300

    SignalR ile chat uygulaması.

---
+++ SignalRApp/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRApp
+{
+    public class ChatHub : Hub
+    {
+        public void HerkeseGonder(string gonderen, string mesaj)
+        {
+            Clients.All.herkeseGonder(gonderen, mesaj, $"{DateTime.Now:g}"); // Javascript geleneğine göre yazılmıştır.
+        }
+    }
+}
